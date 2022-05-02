574e696c3ead1061c0db2f9edb5d4d852b6fce70
aungkyawlin <aungkyawlinmon@gmail.com>
2022-05-02 09:54:12 +0700

    Navigator.pushNamed, Navigator.pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,71 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final _route = <String, WidgetBuilder>{
+    '/page1': (BuildContext context) => MyApp(),
+    '/page2': (BuildContext context) => Page2(),
+    '/page3': (BuildContext context) => MyApp(),
+    '/page4': (BuildContext context) => MyApp(),
+  };
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      routes: _route,
+      title: 'Navigator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Navigator"),
+      home: Page1(),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main Page"),
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/page2');
+          },
+          child: Text("Next Page"),
+        ),
+      ),
+    );
+  }
+}
+
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Page2"),
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("Back"),
         ),
       ),
     );
